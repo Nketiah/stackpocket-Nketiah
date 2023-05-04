1978ec85e8d570fa42bceccfddfe142d73715dc5
Nketiah Joseph <gyatabajoe@gmail.com>
2023-05-04 21:17:05 +0800

    sdsdsds

---
+++ trackNumber.js
@@ -0,0 +1,14 @@
+
+{
+    //usernae
+    //order number
+    //color species
+    //how to play
+    //issue time
+    //multiple mode
+    //bet amount
+    //bet content
+    //winning amount
+    //state
+}
+
